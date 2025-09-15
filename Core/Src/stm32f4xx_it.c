3aed99d81e3ff2eca02e3daa48e224ba7c78b1aa
CHICHU <2085219596@qq.com>
2025-09-15 22:42:08 +0800

    9.14 修改UART中断

--- Core/Src/stm32f4xx_it.c
+++ Core/Src/stm32f4xx_it.c
@@ -269,7 +269,7 @@ void USART3_IRQHandler(void)
   /* USER CODE END USART3_IRQn 0 */
   HAL_UART_IRQHandler(&huart3);
   /* USER CODE BEGIN USART3_IRQn 1 */
-  rc_handleuartirq();
+  Uart3_IRQHandler(&huart3);
   /* USER CODE END USART3_IRQn 1 */
 }
 
