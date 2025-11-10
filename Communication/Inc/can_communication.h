39a2e4515f09fa1267e52d8bbf800bfb15bf5cb9
CHICHU <2085219596@qq.com>
2025-11-10 22:31:01 +0800

    11.10 CAN双板

---
+++ Communication/Inc/can_communication.h
@@ -0,0 +1,13 @@
+//
+// Created by 20852 on 2025/11/10.
+//
+
+#ifndef STARTM3508_CAN_COMMUNICATION_H
+#define STARTM3508_CAN_COMMUNICATION_H
+
+
+class can_communication {
+};
+
+
+#endif //STARTM3508_CAN_COMMUNICATION_H
---
+++ Communication/Src/can_communication.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 20852 on 2025/11/10.
+//
+
+#include "can_communication.h"
