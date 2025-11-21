41c6804b2d1d99eaa1f84e2d4cdc26f34c4b3f27
CHICHU <2085219596@qq.com>
2025-11-21 22:54:23 +0800

    11.21 新增读取运行时间

--- Bsp/Inc/bsp_dwt.h
+++ Bsp/Inc/bsp_dwt.h
@@ -12,7 +12,7 @@ extern "C" {
 #endif
 
     // 初始化 DWT 计数器，返回 0=成功, 1=失败
-    uint8_t DWT_Init(void);
+    uint8_t BSP_DWT_Init(void);
 
     // 微秒级延时
     void DWT_Delay_us(uint32_t us);
@@ -21,7 +21,13 @@ extern "C" {
     void DWT_Delay_ms(uint32_t ms);
 
     // 获取当前 CYCCNT（CPU 时钟计数）
-    static uint32_t DWT_GetCycleCount(void);
+     uint32_t DWT_GetCycleCount(void);
+
+    // 获取自 DWT 启动以来的微秒数
+    uint64_t DWT_GetMicroseconds(void);
+
+    // 获取自 DWT 启动以来的秒数（浮点数）
+    float DWT_GetSeconds(void);
 
 #ifdef __cplusplus
 }
--- Bsp/Src/bsp_dwt.cpp
+++ Bsp/Src/bsp_dwt.cpp
@@ -4,7 +4,7 @@
 
 #include "bsp_dwt.h"
 
-uint8_t DWT_Init(void)
+uint8_t BSP_DWT_Init(void)
 {
     // 1. 使能 DWT
     CoreDebug->DEMCR |= CoreDebug_DEMCR_TRCENA_Msk;
@@ -36,7 +36,26 @@ void DWT_Delay_ms(uint32_t ms)
         DWT_Delay_us(1000);
 }
 
-static uint32_t DWT_GetCycleCount(void)
+ uint32_t DWT_GetCycleCount(void)
 {
     return DWT->CYCCNT;
 }
+
+uint64_t DWT_GetMicroseconds(void)
+{
+    // 当前 DWT 计数器值
+    uint32_t cycles = DWT->CYCCNT;
+
+    // CPU 主频（一般是 400MHz）
+    uint32_t cpu_freq = HAL_RCC_GetHCLKFreq();
+
+    // cycles / (freq / 1e6) = 时间（us）
+    return (uint64_t)cycles * 1000000ull / cpu_freq;
+}
+
+float DWT_GetSeconds(void)
+{
+    // 获取微秒并转成秒
+    return (DWT_GetMicroseconds() * 1e-6f);
+}
+
